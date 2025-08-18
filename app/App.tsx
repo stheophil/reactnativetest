b4ec8da7a21ef4b53243e7030f1e5689fafed8e7
Sebastian Theophil <stheophil@think-cell.com>
2025-08-18 17:24:49 +0200

    Add windows (remove web)
    # Conflicts:
    #	app/package-lock.json
    #	app/package.json

--- app/App.tsx
+++ app/App.tsx
@@ -93,7 +93,7 @@ function App(): React.JSX.Element {
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
             
-            Hello World 🎉
+            Hello World Windows 🎉
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
